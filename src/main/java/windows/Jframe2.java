69afb52e61f79e42a73bcd27b35f6941924397ae
JinhuanLei <lei,jinhuan@uwlax.edu>
2017-10-02 18:12:29 -0500

    first version

--- src/main/java/windows/Jframe2.java
+++ src/main/java/windows/Jframe2.java
@@ -1,13 +1,64 @@
 package windows;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-class Jframe2 extends JFrame {
+class Jframe2 extends JFrame implements ActionListener {
 
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+       Jframe2 test=new Jframe2();
+    }
     public Jframe2(){
         this.setTitle("窗口2");
-        this.setBounds(200, 200, 600, 300);
+        this.setBounds(100, 100, 1500, 900);
+
+        JPanel panel2 = new JPanel();
+        // 添加面板
+        this.add(panel2);
+        /*
+         * 调用用户定义的方法并添加组件到面板
+         */
+        placeComponents(panel2);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         }
 
-    }}
+       public void placeComponents(JPanel panel)
+       {
+           panel.setLayout(null);
+           JButton InitialButton = new JButton("Initial Data");
+           InitialButton.setPreferredSize(new Dimension(100,30));
+           InitialButton.setFont(new   java.awt.Font("Dialog",   1,   20));
+           InitialButton.setBounds(130, 90, 160, 50);
+           panel.add(InitialButton);
+           InitialButton.addActionListener(this);
+
+           JButton ComboButton = new JButton("Add a combination");
+           ComboButton.setPreferredSize(new Dimension(190,30));
+           ComboButton.setFont(new   java.awt.Font("Dialog",   1,   20));
+           ComboButton.setBounds(130, 200, 120, 50);
+           panel.add(ComboButton);
+           ComboButton.addActionListener(this);
+
+           JButton KeyButton = new JButton("Add a new key");
+           KeyButton.setPreferredSize(new Dimension(190,30));
+           KeyButton.setFont(new   java.awt.Font("Dialog",   1,   20));
+           KeyButton.setBounds(130, 300, 190, 50);
+           panel.add(KeyButton);
+           KeyButton.addActionListener(this);
+
+           JButton UpdateKeyButton = new JButton("Update a  key");
+           UpdateKeyButton.setPreferredSize(new Dimension(190,30));
+           UpdateKeyButton.setFont(new   java.awt.Font("Dialog",   1,   20));
+           UpdateKeyButton.setBounds(350, 300, 180, 50);
+           panel.add(UpdateKeyButton);
+           UpdateKeyButton.addActionListener(this);
+       }
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
--- src/main/java/windows/mainpage.java
+++ src/main/java/windows/mainpage.java
@@ -9,30 +9,69 @@ import java.awt.event.WindowEvent;
 
 public class mainpage extends JFrame implements ActionListener {
 
-    JButton jbutton=null;
+
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         mainpage test=new mainpage();
     }
     public mainpage(){
-        jbutton=new JButton("点击打开窗口2");
-        jbutton.addActionListener(this);
-        this.add(jbutton);
+//        jbutton=new JButton("点击打开窗口2");
+//        jbutton.addActionListener(this);
+//        jbutton.setBounds(10, 80, 800, 250);
+       // this.add(jbutton);
         this.setTitle("窗口1");
-        this.setBounds(200, 200, 600, 300);
+        this.setBounds(100, 100, 1500, 900);
+
+        JPanel panel = new JPanel();
+        // 添加面板
+        this.add(panel);
+        /*
+         * 调用用户定义的方法并添加组件到面板
+         */
+        placeComponents(panel);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+
     }
+    private  void placeComponents(JPanel panel) {
 
+        /* 布局部分我们这边不多做介绍
+         * 这边设置布局为 null
+         */
+        panel.setLayout(null);
+
+        // 创建 JLabel
+        JLabel nameLable = new JLabel("Locks&Keys");
+
+        /* 这个方法定义了组件的位置。
+         * setBounds(x, y, width, height)
+         * x 和 y 指定左上角的新位置，由 width 和 height 指定新的大小。
+         */
+        nameLable.setBounds(640,160,300,150);
+        nameLable.setFont(new   java.awt.Font("Dialog",   1,   40));
+        panel.add(nameLable);
+
+
+        JButton startButton = new JButton("Start");
+        startButton.setPreferredSize(new Dimension(50,30));
+        startButton.setFont(new   java.awt.Font("Dialog",   1,   20));
+        startButton.setBounds(700, 560, 80, 50);
+        panel.add(startButton);
+        startButton.addActionListener(this);
+    }
     public void actionPerformed(ActionEvent arg0) {
-        if(arg0.getSource()==jbutton){
-            //创建一个新窗口
-
+        String source = arg0.getActionCommand();
+        if (source.equals("Start")) {
             new Jframe2();
         }
+
+//        if(arg0.getSource()==jbutton){
+//            //创建一个新窗口
+//
+//            new Jframe2();
+//        }
 
     }
 
 }
-//这个类可以单独写在同包下目录 也可以在这直接写
